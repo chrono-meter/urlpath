cd793ee734eff4e2a26d36752677557935f6a5c8
chrono-meter <chrono-meter@gmx.net>
2015-01-24 16:59:14 +0900

    Add setup()'s `install_requires` parameter.

--- setup.py
+++ setup.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import sys
 from setuptools import setup
 import urlpath as target
+
+
+def readme():
+    with open('README.txt') as f:
+        return f.read()
+
+
+install_requires = []
+if sys.version[:3] < '3.4':
+    install_requires.append('pathlib')
+if sys.version[:3] < '3.3':
+    install_requires.append('mock')
 
 setup(
     py_modules=[target.__name__],
@@ -12,7 +25,8 @@ setup(
     url=target.__url__,
     download_url=target.__download_url__,
     description=target.__doc__.strip().splitlines()[0],
-    long_description=open('README.txt').read(),
+    long_description=readme(),
     classifiers=target.__classifiers__,
     license=target.__license__,
+    install_requires=install_requires,
 )
