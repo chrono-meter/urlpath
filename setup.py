4a2cebad11157a5b22d3fd8f7b4ec5b73e0b5d6f
chrono-meter <chrono-meter@gmx.net>
2015-12-05 06:47:52 +0900

    Try to fix travis

--- setup.py
+++ setup.py
@@ -1,13 +1,37 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import sys
+import os
+import ast
 from setuptools import setup
-import urlpath as target
 
 
 def readme():
     with open('README.rst') as f:
         return f.read()
+
+
+def get_meta(filename):
+    result = {
+        '__file__': filename,
+        '__name__': os.path.splitext(os.path.basename(filename))[0],
+        '__package__': '',
+    }
+
+    with open(filename) as fp:
+        root = ast.parse(fp.read(), fp.name)
+
+    result['__doc__'] = ast.get_docstring(root)
+
+    for node in ast.iter_child_nodes(root):
+        if isinstance(node, ast.Assign):
+            for target in node.targets:
+                try:
+                    result[target.id] = ast.literal_eval(node.value)
+                except ValueError:
+                    pass
+
+    return result
 
 
 install_requires = ['requests']
@@ -16,17 +40,19 @@ if sys.version[:3] < '3.4':
 if sys.version[:3] < '3.3':
     install_requires.append('mock')
 
+meta = get_meta('urlpath.py')
+
 setup(
-    py_modules=[target.__name__],
-    name=target.__name__,
-    version=target.__version__,
-    author=target.__author__,
-    author_email=target.__author_email__,
-    url=target.__url__,
-    download_url=target.__download_url__,
-    description=target.__doc__.strip().splitlines()[0],
+    py_modules=[meta['__name__']],
+    name=meta['__name__'],
+    version=meta['__version__'],
+    author=meta['__author__'],
+    author_email=meta['__author_email__'],
+    url=meta['__url__'],
+    download_url=meta['__download_url__'],
+    description=meta['__doc__'].strip().splitlines()[0],
     long_description=readme(),
-    classifiers=target.__classifiers__,
-    license=target.__license__,
+    classifiers=meta['__classifiers__'],
+    license=meta['__license__'],
     install_requires=install_requires,
 )
